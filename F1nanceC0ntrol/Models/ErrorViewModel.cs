0d4e6682f7da038f9f329fdc31ac02901062aadf
Lucas Rocha <lucas.oct8@gmail.com>
2025-05-12 15:27:00 -0300

    start project

---
+++ F1nanceC0ntrol/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace F1nanceC0ntrol.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
